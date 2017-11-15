1d8eb49d17c41a3ae514fd0a9d62628743404aa8
dalanao <dal437@nyu.edu>
2017-11-15 01:13:31 -0500

    Modified code and working on game engine.

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -10,40 +10,46 @@ function main(){
 }
 
 class Card {
-  constructor(number, suit) {
-    this.number = number;
-    this.suit = suit;
-  }
+	constructor(number, suit) {
+		this.number = number;
+		this.suit = suit;
+	}
 }
 
 class Deck {
-  constructor() {
-    this.numbers = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
-    this.suits = ["hearts", "clubs", "spades", "diamonds"];
-    this.deck = [];
-  }
-  cleanDeck() {
-    this.deck = [];
-  }
-  generateDeck() {
-    this.cleanDeck();
-    for (let suitIndex in suits) {
-      for (let numberIndex in numbers) {
-        const card = new Card(this.numbers);
-        this.addCard(card);
-      }
-    }
-  }
-  addCard(card) {
-    this.deck.push(cards);
-  }
-  shuffleDeck() {
-    for (let i = this.card.length - 1; i > 0; i--){
+	constructor() {
+		this.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+		this.suits = ['hearts', 'clubs', 'spades', 'diamonds'];
+		this.cards = [];
+	}
+
+	addCard(card) {
+		this.cards.push(card);
+	}
+
+	shuffle() {
+    for (let i = this.cards.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      const x = this.cards[i];
+      this.swapCardsPosition(i, j);
+      /* const x = this.cards[i];
       this.cards[i] = this.cards[j];
-      this.cards[j] = x;
+      this.cards[j] = x;*/
     }
   }
-  });
+
+  setInitialState(intialNumbers = []) {
+    // Generate random suits that doesn't repeat
+    const initialCards = [];
+    for (let i = 0; i < intialNumbers.length; i++) {
+      let randomSuit = this.suits[Math.round(Math.random() * 10) % 4];
+      let c = new Card(intialNumbers[i], randomSuit);
+      while (initialCards.find(x => x.number === c.number && x.suit === c.suit)) {
+        randomSuit = this.suits[Math.round(Math.random() * 10) % 4];
+        c = new Card(intialNumbers[i], randomSuit);
+      }
+      initialCards.push(c);
+      const ic = this.findCardPosition(c);
+      this.swapCardsPosition(i, ic);
+    }
+  }
 }
